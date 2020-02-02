4cd456e53b07667081bdb44a63e1865ed118b775
gongwei <1325075688@qq.com>
2020-02-03 00:51:24 +0800

    改名字为昆明,测试第二次改名词 添加云南丽江模块

--- China/src/com/biggw/YunNanKunMing.java
+++ China/src/com/biggw/YunNanKunMing.java
@@ -1,6 +1,6 @@
 package com.biggw;
 
-public class YunNan {
+public class YunNanKunMing {
     public static void main(String[] args) {
         System.out.println("亚洲-云南模块开发完成");
     }
---
+++ China/src/com/biggw/YunNanLiJiang.java
@@ -0,0 +1,7 @@
+package com.biggw;
+
+public class YunNanLiJiang {
+    public static void main(String[] args) {
+        System.out.println("云南丽江");
+    }
+}
